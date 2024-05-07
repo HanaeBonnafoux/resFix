355a0d0fb30e2855f835880d3644de85dc6fcda4
poeym <poymakes@gmail.com>
2024-05-07 16:16:02 +0200

    Translated the comments and the function names to English.

--- Reservoir3.py
+++ Reservoir3.py
@@ -1,17 +1,18 @@
-from Tkinter import *
+from tkinter import *
 from random import *
 from math import*
 
-# Variable globales 
+# Global variables 
 CANVAS_HEIGHT, CANVAS_WIDTH = 500, 700
 
 canvasHeight=500
 canvasWidth=750
 
-listeParticules=[]
-Ec=0
-nbrParticules=0
-P=0#nrT
+listParticles=[]
+Ek=0
+numberParticle=0
+P=0
+#nrT --- ok, what is nrt. Need to figure it out
 flag=1
 
 nbrLabel = None
@@ -20,25 +21,26 @@ reservoir = None
 
 # FONCTIONS
 
-def distanceAuCarre(xA,yA,xB,yB):
+def distanceSquared(xA,yA,xB,yB):
     d=(xA-xB)**2+(yA-yB)**2
     return d
 
-def calculerEnergieCinetique():
-    global Ec
+def calculateKineticEnergy():
+    global Ek
     if flag==1:
-        Ec=0
-        for p in listeParticules :
-            Ec+=0.5*p[4]*(p[2]**2+p[3]**2)
+        Ek=0
+        for p in listParticles :
+            Ek+=0.5*p[4]*(p[2]**2+p[3]**2)
     if flag==0 :
-        Ec=0
-    return Ec
+        Ek=0
+    return Ek
+#There is probably a problem here because the kinetic energy returns a strange result
 
-def creerParticuleValide():
+def createValidParticle():
     
-# Creation
+# Creating a particle
     
-    global nbrParticules,flag
+    global numberParticle,flag
     flag=1
     size=1.*randint(15,55)
     mass=size
@@ -47,61 +49,61 @@ def creerParticuleValide():
     coordy=1.*randint(0+size,canvasHeight-size)
     color=choice(['Red','Orange','Yellow','Green','Cyan','Blue','Purple','Magenta'])
     index=reservoir.create_oval(coordx,coordy,coordx+size,coordy+size,fill=color)
-    particule=[coordx,coordy,dX,dY,mass,index]
-    listeParticules.append(particule)
-    nbrParticules+=1
+    particle=[coordx,coordy,dX,dY,mass,index]
+    listParticles.append(particle)
+    numberParticle+=1
 
 # Test overlap
 
-    for p in listeParticules:  
-        if p!=particule and distanceAuCarre(p[0],p[1],particule[0],particule[1])<=(p[4]+particule[4])**2 :
-            reservoir.delete(particule[5])
-            l=len(listeParticules)-1
-            del listeParticules[l]
-            creerParticuleValide()
+    for p in listParticles:  
+        if p!=particle and distanceSquared(p[0],p[1],particle[0],particle[1])<=(p[4]+particle[4])**2 :
+            reservoir.delete(particle[5])
+            l=len(listParticles)-1
+            del listParticles[l]
+            createValidParticle()
             break
-    return nbrParticules
+    return numberParticle
 
-def plusUn():
-    global nbrParticules
-    nbrParticules+=1
+def plusOne():
+    global numberParticle
+    numberParticle+=1
     
 
 def fcombine():
-    creerParticuleValide()
-    #plusUn()
-    #lol en fait ca marche pas
+    createValidParticle()
+    #plusOne()
+    #lol it is not working
         
 def move ():
-    nbrLabel.config(text='Nombre de particules=' + str(nbrParticules))
-    Ec = int(calculerEnergieCinetique())
-    EcLabel.config(text='Ec=' + str(Ec))
+    nbrLabel.config(text='Number of particles =' + str(numberParticle))
+    Ek = int(calculateKineticEnergy())
+    EcLabel.config(text='Ek=' + str(Ek))
     if flag==1:
-        for particule in listeParticules:
-            posX=particule[0]
-            posY=particule[1]
-            particule[0] += particule[2]
-            particule[1] += particule[3]
-            if particule[0]-particule[4]<=0 or particule[0]+particule[4]>=canvasWidth :
-                particule[2]*=-1
-                particule[0]=posX
-                particule[1]=posY
-            if particule[1]-particule[4]<=0 or particule[1]+particule[4]>=canvasHeight :
-                particule[3]*=-1
-                particule[0]=posX
-                particule[1]=posY
-            for otherParticule in listeParticules :
-                if otherParticule != particule and distanceAuCarre(particule[0],particule[1],otherParticule[0],otherParticule[1])<=(particule[4]+otherParticule[4])**2 :
-                    particule[0]=posX
-                    particule[1]=posY
-                    nx=(particule[0]-otherParticule[0])/sqrt((particule[0]-otherParticule[0])**2+(particule[1]-otherParticule[1])**2)
-                    ny=(particule[1]-otherParticule[1])/sqrt((particule[0]-otherParticule[0])**2+(particule[1]-otherParticule[1])**2)
+        for particle in listParticles:
+            posX=particle[0]
+            posY=particle[1]
+            particle[0] += particle[2]
+            particle[1] += particle[3]
+            if particle[0]-particle[4]<=0 or particle[0]+particle[4]>=canvasWidth :
+                particle[2]*=-1
+                particle[0]=posX
+                particle[1]=posY
+            if particle[1]-particle[4]<=0 or particle[1]+particle[4]>=canvasHeight :
+                particle[3]*=-1
+                particle[0]=posX
+                particle[1]=posY
+            for otherParticule in listParticles :
+                if otherParticule != particle and distanceSquared(particle[0],particle[1],otherParticule[0],otherParticule[1])<=(particle[4]+otherParticule[4])**2 :
+                    particle[0]=posX
+                    particle[1]=posY
+                    nx=(particle[0]-otherParticule[0])/sqrt((particle[0]-otherParticule[0])**2+(particle[1]-otherParticule[1])**2)
+                    ny=(particle[1]-otherParticule[1])/sqrt((particle[0]-otherParticule[0])**2+(particle[1]-otherParticule[1])**2)
                     tx=-ny
                     ty=nx
-                    m1=particule[4]
+                    m1=particle[4]
                     m2=otherParticule[4]
-                    v1x=particule[2]
-                    v1y=particule[3]
+                    v1x=particle[2]
+                    v1y=particle[3]
                     v2x=otherParticule[2]
                     v2y=otherParticule[3]
                     v1n=v1x*nx + v1y*ny
@@ -110,11 +112,11 @@ def move ():
                     v2t=v2x*tx + v2y*ty
                     v1nn=((m1-m2)/(m1+m2))*v1n + ((2*m2)/(m1+m2))*v2n
                     v2nn=((2*m1)/(m1+m2))*v1n + ((m2-m1)/(m1+m2))*v2n
-                    particule[2]=v1nn*nx - v1t*ny
+                    particle[2]=v1nn*nx - v1t*ny
                     otherParticule[2]=v2nn*nx - v2t*ny
                     otherParticule[3]=v2t*nx + v2nn*ny
-                    particule[3]=v1t*nx + v1nn*ny
-            reservoir.coords(particule[5],particule[0]-particule[4],particule[1]-particule[4],particule[0]+particule[4],particule[1]+particule[4])
+                    particle[3]=v1t*nx + v1nn*ny
+            reservoir.coords(particle[5],particle[0]-particle[4],particle[1]-particle[4],particle[0]+particle[4],particle[1]+particle[4])
     reservoir.after(10,move)
 
 
@@ -123,13 +125,13 @@ def stop_it():
     global flag
     flag=0
 
-def raz():
-    global flag, nbrParticules,listeParticules, Ec
+def reset():
+    global flag, numberParticle,listParticles, Ek
     flag=0
-    nbrParticules=0
-    for particule in listeParticules :
-        reservoir.delete(particule[5])
-    del listeParticules[:]
+    numberParticle=0
+    for particle in listParticles :
+        reservoir.delete(particle[5])
+    del listParticles[:]
 
     
 def main():
@@ -139,30 +141,29 @@ def main():
 
     mainWindow=Tk()
     mainWindow.geometry('1000x545')
-    mainWindow.title('RESERVOIR DE PARTICULES')
+    mainWindow.title('PARTICLE RESERVOIR')
 
     reservoir=Canvas(mainWindow,bg='light grey',height=canvasHeight,width=canvasWidth)
     reservoir.place(x=0,y=0)
 
-    createButton=Button(mainWindow,text='+1 Particules ',command= fcombine)
+    createButton=Button(mainWindow,text='+1 Particles ',command= fcombine)
     createButton.place(x=150,y=510)
 
-    rapportButton=Button(mainWindow,text=' Arret ', command=stop_it)
+    rapportButton=Button(mainWindow,text=' Stop ', command=stop_it)
     rapportButton.place(x=345,y=510)
 
-    razButton=Button(mainWindow,text='Remise a zero ', command=raz)
+    razButton=Button(mainWindow,text='Reset ', command=reset)
     razButton.place(x=550,y=510)
 
-    # data et labels
+    # data and labels
 
-
-    nbrLabel=Label(mainWindow,text='Nbr particules = ' + str(nbrParticules))
+    nbrLabel=Label(mainWindow,text='Number of particles = ' + str(numberParticle))
     nbrLabel.place(x=canvasWidth+25,y=25)
 
-    EcLabel=Label(mainWindow,text='Ec = ' + str(Ec))
+    EcLabel=Label(mainWindow,text='Kinetic Energy = ' + str(Ek))
     EcLabel.place(x=canvasWidth+25,y=50)
 
-    print(Ec)
+    print(Ek)
 
     move()
     mainWindow.mainloop()
